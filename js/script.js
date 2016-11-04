37ad48d7d99f90e74e5532909e7d49e40a279ea4
blinebau <blinebau@uci.edu>
2016-11-04 13:58:43 -0700

    Finished for Midterm Req. Working on hosting site.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function()
 {
-	$.scrollify({
+/*	$.scrollify({
 		section : "section",
 		sectionName : "section-name",
 		interstitialSection : "",
@@ -10,41 +10,44 @@ $(document).ready(function()
 		scrollSpeed: 1100,
 		offset : 0,
 		scrollbars: true,
-		target:"html,body",
 		standardScrollElements: false,
-		setHeights: true,
+		setHeights: false,
 		overflowScroll: true,
 		before:function() {},
 		after:function() {},
 		afterResize:function() {},
 		afterRender:function() {}
 	});
-		
-	$('#scroll-down').on('click', function(event) {
-		event.preventDefault();
-		$.scrollify.move("#about-section");
-	});
 
 	$('#nav_links').on('click', 'a', function(event) {
 		event.preventDefault();
 		var ref = $(this).attr('href');
 		$.scrollify.move(ref);
-	});
+	});*/
 
-	var navOffSet = $('#about').offset().top;
+	var navOffSet = $('#site-nav').offset().top;
 
 	$(window).scroll(function() {
-		console.log("ran");
-		if($(window).scrollTop() > navOffSet) {
-			$('#nav_links').css({ position: 'fixed', top: '0'});
+		if($(window).scrollTop() >= navOffSet) {
+			$('#site-nav').css({ position: 'fixed', top: '0'});
 		}
 		if($(window).scrollTop() < navOffSet) {
-			$('#nav_links').css({ position: 'absolute', top: '100%'});
+			$('#site-nav').css({ position: 'absolute', top: '100%'});
 		}
 	});
 
 	$('#hamburger-nav').on('click', function(event) {
 		event.preventDefault();
-		$('#nav_links').toggleClass('toggle')
+		$('#nav_links').toggleClass('toggle');
+	});
+
+	$('.degree').on('click', function(event) {
+		$(this).next().toggleClass('show');
+		$("i", this).toggleClass('fa-caret-down fa-caret-right');
+	});
+
+	$('.proj-desc').on('click', function(event) {
+		$(this).next().toggleClass('show');
+		$("i", this).toggleClass('fa-caret-down fa-caret-right');
 	})
 });
